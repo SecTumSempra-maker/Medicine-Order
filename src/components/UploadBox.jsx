e1e036109210c34805ccc14e6b075656dd2cd6c9
Siddharth <siddharthlegend12@gmail.com>
2025-06-10 14:52:35 +0000

    adding display boc functional

--- src/components/UploadBox.jsx
+++ src/components/UploadBox.jsx
@@ -1,14 +1,33 @@
 import classes from "./UploadBox.module.css";
+import { useState } from "react";
 
 function Upload() {
+  const [changedText, setChangedText] = useState("");
+  const [changedisplay, setChangedDisplay] = useState("");
+  function handleTextChange(event) {
+    setChangedText(event.target.value);
+  }
+
+  function handleDisplay(event) {
+    event.preventDefault();
+    setChangedDisplay(changedText);
+  }
   return (
     <div className={classes.wrapper}>
       <form className={classes.form}>
         <p>
           <label htmlFor="textbody">Medicine Prescription</label>
-          <textarea id="textbody" required rows={1} />
+          <textarea
+            id="textbody"
+            required
+            rows={1}
+            onChange={handleTextChange}
+          />
         </p>
-        <button type="submit">Upload</button>
+
+        <p>{changedisplay}</p>
+
+        <button onClick={handleDisplay}>Upload</button>
       </form>
     </div>
   );
